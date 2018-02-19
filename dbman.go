3d46d8903d8f44695cd0bd4ed4e727445e0999c6
Sam Lerner <lerner98@gmail.com>
2018-02-19 17:15:06 -0500

    fix for silly admin bug

--- dbman.go
+++ dbman.go
@@ -82,8 +82,8 @@ func getAnswers(db *sql.DB) [][]string {
 	defer rows.Close()
 
 	for rows.Next() {
-		row := make([]interface{}, 19)
-		if err := rows.Scan(&row[0], &row[1], &row[2], &row[3], &row[4], &row[5], &row[6], &row[7], &row[8], &row[9], &row[10], &row[11], &row[12], &row[13], &row[14], &row[15], &row[16], &row[17], &row[18]); err != nil {
+		row := make([]interface{}, 20)
+		if err := rows.Scan(&row[0], &row[1], &row[2], &row[3], &row[4], &row[5], &row[6], &row[7], &row[8], &row[9], &row[10], &row[11], &row[12], &row[13], &row[14], &row[15], &row[16], &row[17], &row[18], &row[19]); err != nil {
 			log.Fatal("error scanning row", err)
 			return answers
 		}
