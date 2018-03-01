412154717ba458558849d32542545755cbf1cb87
Sam Lerner <lerner98@gmail.com>
2018-03-01 11:52:42 -0500

    actually wrapping strings

--- routes.go
+++ routes.go
@@ -153,7 +153,7 @@ func seeAll(w http.ResponseWriter, r *http.Request) {
 	var buffer bytes.Buffer
 
 	answers := getAnswers(db)
-	buffer.WriteString("article_id, \"text\", id, l/m, wave, subj_id, user_id, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12\n")
+	buffer.WriteString("article_id, text, id, l/m, wave, subj_id, user_id, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12\n")
 	for _, answer := range answers {
 		buffer.WriteString(strings.Join(answer, ", "))
 		buffer.WriteString("\n")
